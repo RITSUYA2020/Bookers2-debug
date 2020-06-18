6399b220768c4bfda7c28acecfbb37490b48d095
vagrant <vagrant@localhost.localdomain>
2020-06-18 12:50:41 +0000

    [Updte]book_comments_controllerのcreateアクション

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,21 @@
 class BookCommentsController < ApplicationController
+
+	before_action :authenticate_user!
+
 	def create
-		book = Book.find(params[:book_id])
-		comment = BookComment.new(book_comment_params)
-		comment.user_id = current_user.id
-		comment.book_id = book.id
-		comment.save
-		redirect_to book_path(book)
+		@book = Book.find(params[:book_id])
+		@new_book_comment = BookComment.new(book_comment_params)
+		@new_book_comment.user_id = current_user.id
+		@new_book_comment.book_id = @book.id
+		if @new_book_comment.save
+			flash[:success] = "Comment was successfully created."
+			redirect_to book_path(@book)
+		else
+			@book_comments = @book.book_comments
+			@user = current_user
+			@new_book = Book.new
+			render '/books/show'
+		end
 	end
 
 	def destroy
