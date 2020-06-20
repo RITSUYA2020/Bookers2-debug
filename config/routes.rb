723a847e0757240b28e721a7cb69ed29b7e04c85
vagrant <vagrant@localhost.localdomain>
2020-06-20 07:38:48 +0000

    [Finish]応用課題4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show, :index, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update] do
+  	resource :relationships, only: [:create, :destroy]
+  	get :follows
+  	get :followers
+  end
   resources :books
   root 'home#top'
   get 'home/about' => 'home#about'
